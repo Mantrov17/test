fa36f441290b098e23626c701b2ee3d75586db46
stepa <Stepanmantrov12.05@gmail.com>
2022-10-15 21:52:33 +0300

    Add new function

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')
 
-
+def foo():
+    print("Foo!")
 
 if __name__ == '__main__':
     print_hi('PyCharm')
